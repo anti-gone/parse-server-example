6efa619177b23dca4c6f1584840e34d4ce54d96e
LMUEC <LMUEC@users.noreply.github.com>
2016-03-18 21:42:44 +0100

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ var api = new ParseServer({
   cloud:  __dirname + '/cloud/main.js',
   appId: 'ck6vEY86u2hwvlZiwYjbxW9zcmkVrq3MBRIHetgY',
   masterKey:  'SLZtu100g9idpw4UkCVHckvfzEJlBuMGBNnXA3sh' //Add your master key here. Keep it secret!
-  //,serverURL: process.env.SERVER_URL || 'http://localhost:1337'  // Don't forget to change to https if needed
+  ,serverURL: process.env.SERVER_URL || 'http://localhost:1337'  // Don't forget to change to https if needed
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
