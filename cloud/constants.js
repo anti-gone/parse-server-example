b66fa284a02ce28d1f490a8f2e9610e0c329bbec
Charlotte Prieß <priess@cip.ifi.lmu.de>
2016-03-19 15:48:36 +0100

    added constants.js

---
+++ cloud/constants.js
@@ -0,0 +1,128 @@
+//*** Parse.User Class
+//********************
+module.exports.UNICORN_USER_FULL_NAME					= "fullname";
+module.exports.UNICORN_USER_FULL_NAME_LOWER				= "fullname_lower";
+module.exports.UNICORN_USER_FIRST_NAME					= "firstName";
+module.exports.UNICORN_USER_PICTURE_FILE				= "picture";
+module.exports.UNICORN_USER_THUMBNAIL_FILE				= "thumbnail";
+module.exports.UNICORN_USER_PRIVATE_USER_DATA_POINTER	= "privateData";
+module.exports.UNICORN_USER_IS_STUDENT					= "isStudent";
+module.exports.UNICORN_USER_UNIVERSITY_POINTER			= "university";
+module.exports.UNICORN_USER_UNIVERSITY_NAME				= "universityName";
+module.exports.UNICORN_USER_SKILLS_POINTER_ARRAY		= "skills";
+module.exports.UNICORN_USER_STARTUP_POINTER				= "startup";
+module.exports.UNICORN_USER_UNI_OR_STARTUP_LOWER		= "uniOrStartupLower";
+module.exports.UNICORN_USER_PUSH_OPTION					= "pushOption";
+module.exports.UNICORN_USER_EVENTS_POINTER_ARRAY		= "events";
+module.exports.UNICORN_USER_TRACKS_POINTER_ARRAY		= "tracks";
+module.exports.UNICORN_USER_PROFILE_UPDATE_DATE			= "profileUpdateDate";
+
+
+//*** Parse.Installation Class
+//****************************
+module.exports.UNICORN_INSTALLATION_USER_POINTER			= "user";
+module.exports.UNICORN_INSTALLATION_DEVICE_TYPE				= "deviceType";
+module.exports.UNICORN_INSTALLATION_APP_IDENTIFIER			= "appIdentifier";
+module.exports.UNICORN_INSTALLATION_APP_NAME				= "unicorn";
+module.exports.UNICORN_INSTALLATION_APP_VERSION				= "appVersion";
+module.exports.UNICORN_INSTALLATION_BADGE_NUMBER			= "badge";
+module.exports.UNICORN_INSTALLATION_CHANNELS_ARRAY			= "channels";
+module.exports.UNICORN_INSTALLATION_DEVICE_TOKEN			= "deviceToken";
+module.exports.UNICORN_INSTALLATION_DEVICE_TOKEN_MODIFIED	= "deviceTokenLastModified";
+module.exports.UNICORN_INSTALLATION_INSTALLATION_ID			= "installationId";
+module.exports.UNICORN_INSTALLATION_PARSE_VERSION			= "parseVersion";
+module.exports.UNICORN_INSTALLATION_PUSH_TYPE				= "pushType";
+module.exports.UNICORN_INSTALLATION_TIME_ZONE				= "timeZone";
+module.exports.UNICORN_INSTALLATION_LOCALE_IDENTIFIER		= "localeIdentifier";
+
+
+//*** BlockedUser Class
+//*********************
+module.exports.UNICORN_BLOCKED_USER_CLASS_NAME				= "BlockedUser";
+
+module.exports.UNICORN_BLOCKED_USER_BLOCKED_USER_POINTER	= "blockedUser";
+module.exports.UNICORN_BLOCKED_USER_BLOCKING_USER_POINTER	= "blockingUser";
+
+
+//*** Chat Class
+//**************
+module.exports.UNICORN_CHAT_CLASS_NAME		= "Chat";
+
+module.exports.UNICORN_CHAT_PICTURE_FILE	= "picture";
+module.exports.UNICORN_CHAT_ROOM_ID			= "roomId";
+module.exports.UNICORN_CHAT_TEXT			= "text";
+module.exports.UNICORN_CHAT_USER_POINTER	= "user";
+
+
+//*** Event Class
+//***************
+module.exports.UNICORN_EVENT_CLASS_NAME					= "Event";
+
+module.exports.UNICORN_EVENT_EVENT_NAME					= "eventName";
+module.exports.UNICORN_EVENT_EVENT_DATE					= "eventDate";
+module.exports.UNICORN_EVENT_EVENT_END_DATE				= "eventEndDate";
+module.exports.UNICORN_EVENT_EVENT_LOCATION				= "eventLocation";
+module.exports.UNICORN_EVENT_EVENT_DESCRIPTION			= "eventDescription";
+module.exports.UNICORN_EVENT_EVENT_ROOM					= "eventLocationRoom";
+module.exports.UNICORN_EVENT_EVENT_PICTURE				= "eventPicture";
+module.exports.UNICORN_EVENT_ATTENDANTS_NUMBER			= "numberAttendants";
+module.exports.UNICORN_EVENT_ATTENDANTS_POINTER_ARRAY	= "userAttendants";
+
+
+//*** Messages Class
+//******************
+module.exports.UNICORN_MESSAGES_CLASS_NAME			= "Messages";
+module.exports.UNICORN_MESSAGES_COUNTER				= "counter";
+module.exports.UNICORN_MESSAGES_LAST_MESSAGE		= "lastMessage";
+module.exports.UNICORN_MESSAGES_ROOM_ID				= "roomId";
+module.exports.UNICORN_MESSAGES_USER_POINTER		= "user";
+module.exports.UNICORN_MESSAGES_TO_USER_POINTER		= "toUser";
+
+
+//*** PrivateUserData Class
+//*************************
+module.exports.UNICORN_PRIVATE_USER_DATA_CLASS_NAME		= "PrivateUserData";
+
+module.exports.UNICORN_PRIVATE_USER_DATA_EMAIL			= "email";
+module.exports.UNICORN_PRIVATE_USER_DATA_FACEBOOK_ID	= "facebookId";
+module.exports.UNICORN_PRIVATE_USER_DATA_GENDER			= "gender";
+module.exports.UNICORN_PRIVATE_USER_DATA_AGE_RANGE		= "ageRange";
+
+
+//*** Skills Class
+//****************
+module.exports.UNICORN_SKILLS_CLASS_NAME			= "Skills";
+
+module.exports.UNICORN_SKILLS_POSITION				= "position";
+module.exports.UNICORN_SKILLS_SKILL_NAME			= "skillName";
+module.exports.UNICORN_SKILLS_SKILL_NAME_LOWER		= "skillNameLower";
+module.exports.UNICORN_SKILLS_SKILL_PICTURE_FILE	= "skillPicture";
+
+
+//*** Startup Class
+//*****************
+module.exports.UNICORN_STARTUP_CLASS_NAME			= "Startup";
+
+module.exports.UNICORN_STARTUP_STARTUP_NAME			= "startupName";
+module.exports.UNICORN_STARTUP_STARTUP_NAME_LOWER	= "startupNameLower";
+
+
+//*** University Class
+//********************
+module.exports.UNICORN_UNIVERSITY_CLASS_NAME		= "University";
+
+module.exports.UNICORN_UNIVERSITY_UNIVERSITY_NAME	= "universityName";
+
+
+//*** Track Class
+//***************
+module.exports.UNICORN_TRACK_CLASS_NAME			= "Track";
+
+module.exports.UNICORN_TRACK_EVENT_POINTER		= "event";
+module.exports.UNICORN_TRACK_ATTENDANTS_NUMBER	= "numberAttendants";
+module.exports.UNICORN_TRACK_TRACK_NAME			= "trackName";
+module.exports.UNICORN_TRACK_TRACK_DESCRIPTION	= "trackDescription";
+module.exports.UNICORN_TRACK_TRACK_LOCATION		= "trackLocation";
+module.exports.UNICORN_TRACK_START_DATE			= "startDate";
+module.exports.UNICORN_TRACK_END_DATE			= "endDate";
+module.exports.UNICORN_TRACK_TRACK_PICTURE		= "trackPicture";
