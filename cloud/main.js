3f59b7e6ce17c7386f680541b0c031a97cf2c6fc
Charlotte Prieß <priess@cip.ifi.lmu.de>
2016-03-22 14:52:11 +0100

    jfkldsjf

--- cloud/main.js
+++ cloud/main.js
@@ -132,7 +132,7 @@ Parse.Cloud.define("sendMessagePush", function(request, response) {
 		},
 		error: function(error) {
 			
-			console.log("push failed!");
+			console.log("push failed!"+ error.code + " : " + error.message+" "+data);
 			
 			throw "Got an error " + error.code + " : " + error.message;
 			response.error("push failed!" + error.code + " : " + error.message+" "+data);
